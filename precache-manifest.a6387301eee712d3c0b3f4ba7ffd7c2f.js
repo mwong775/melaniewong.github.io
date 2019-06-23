db74beed00f60b3d3cbf7323c7ef2d8180a76c53
Melanie Wong <mwong775@gmail.com>
2019-06-22 23:34:04 -0700

    Updates

---
+++ precache-manifest.a6387301eee712d3c0b3f4ba7ffd7c2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249ac96ed845c2d13e5a6919739ef8ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddbbb7339db22294da56",
+    "url": "/static/css/main.a201c733.chunk.css"
+  },
+  {
+    "revision": "e77ca7bf7d9e44dbfa63",
+    "url": "/static/js/2.d3be6247.chunk.js"
+  },
+  {
+    "revision": "ddbbb7339db22294da56",
+    "url": "/static/js/main.cfa7a6ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
