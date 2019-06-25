5e1a39a5c506e26ef392ef7f72a7c869a3f1b9b0
Melanie Wong <mwong775@gmail.com>
2019-06-24 23:10:33 -0700

    reorganized but tabs rip :(

--- src/App.js
+++ src/App.js
@@ -1,134 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+//import * as React from "react";
 //import logo from './logo.svg';
 import './App.css';
 // import particlesJS from 'particles-js';
-import Particles from 'react-particles-js';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Home} from "./Home/Home";
 import Carousel from 'react-bootstrap/Carousel';
 
-
-const particleOpt = {
-              "particles": {
-                  "number": {
-                  "value": 50
-              },
-              "size": {
-                "value": 3
-              }
-          },
-          "interactivity": {
-              "events": {
-                  "onhover": {
-                      "enable": true,
-                      "mode": "repulse"
-              }
-          }
-      }
-}
-
-class ControlledCarousel extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.handleSelect = this.handleSelect.bind(this);
-
-    this.state = {
-      index: 0,
-      direction: null,
-    };
-  }
-
-  handleSelect(selectedIndex, e) {
-    this.setState({
-      index: selectedIndex,
-      direction: e.direction,
-    });
-  }
-
+export class App extends React.Component {
   render() {
-    const { index, direction } = this.state;
+      return (
+        <Router basename={process ? process.env.PUBLIC_URL : ""}>
+          <div>
+              <header>
 
-    return (
-      <Carousel
-        activeIndex={index}
-        direction={direction}
-        onSelect={this.handleSelect}
-      >
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    );
+              </header>
+              <div className="container mt-5">
+                  <Switch>
+                      <Route path="/" exact component={Home} />
+                  </Switch>
+              </div>
+              <footer className="mb-5"/>
+          </div>
+      </Router>
+      );
   }
 }
 
-
-function App() {
-//   particlesJS.load('particles-js', 'assets/particles.json', function() {
-//   console.log('callback - particles.js config loaded');
-// });
-
-  return (
-
-    <div className="App"> hi
-        <Particles
-    params={particleOpt} />
-    // <ControlledCarousel/>
-
-    </div>
-     
-      // <header className="App-header">
-      //   <img src={logo} className="App-logo" alt="logo" />
-      //   <p>
-      //     Edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      //   <a
-      //     className="App-link"
-      //     href="https://reactjs.org"
-      //     target="_blank"
-      //     rel="noopener noreferrer"
-      //   >
-      //     Learn React
-      //   </a>
-      // </header>
-    // </div>
-  );
-}
 
 
 export default App;
