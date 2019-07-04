b59f37dabe930fabff0eba65506eab9329a2de9e
Melanie Wong <mwong775@gmail.com>
2019-07-03 22:52:49 -0700

    added table with pagination

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import * as React from "react";
 //import logo from './logo.svg';
 //import './App.css';
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,26 +1,16 @@
 import * as React from "react";
 import Particles from 'react-particles-js'; // LEGIT ONE
-import {Tab, Tabs} from "react-bootstrap";
 import './Home.css';
+import {Nav} from "../Nav/Nav";
+import DatatablePage from "./DataTable";
 
 
 export class Home extends React.Component {
 	render() {
 		return (
-			<div>
+			<div className="main">
 				<h1>Home Page!</h1>
-				<Tabs defaultActiveKey="home" id="main-tabs">
-					<Tab eventKey="home" title="Home">
-						<div className="border">
-							<p>this is a paragraph.</p>
-						</div>
-					</Tab>
-					<Tab eventKey="stuff" title="Stuff">
-						<div className="border">
-							<p>here's another paragraph to take up space</p>
-						</div>
-					</Tab>
-				</Tabs>
+				<DatatablePage/>
 				<Particles params={particleOpt} />
 			</div>
 			);
