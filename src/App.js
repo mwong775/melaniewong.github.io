93371a087446c86938878622172f09df44af334a
Melanie Wong <mwong775@gmail.com>
2019-10-04 00:23:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ export class App extends React.Component {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 const particleOpt = {
               "particles": {
           "number": {
@@ -75,6 +76,8 @@ const particleOpt = {
 }
 
 
+=======
+>>>>>>> parent of e01213d... added some legit data to table, fixed particles to fill page :D
 =======
 >>>>>>> parent of e01213d... added some legit data to table, fixed particles to fill page :D
 =======
