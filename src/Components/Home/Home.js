67ff79c1fe567e39769a771044a992672bbfcc81
mwong775 <mwong775@gmail.com>
2020-02-16 15:43:04 -0800

    remove blue mountains, fix target blank security

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import './Home.css';
-import blueMountains from '../Photos/images/blue_mountains.jpg';
 import {Link} from 'react-router-dom'; 
 import Button from '@material-ui/core/Button';
 
@@ -8,30 +7,20 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div className="home">
-      <div className="mtn">
-        <img
-          className="w-100"
-          src={blueMountains}
-          alt="Blue Mountains - could not load :("
-          />
-          <div className="mtn-caption">
-              <h3 style={{fontSize: '3vw'}}>Melanie Wong</h3>
-              <h4 style={{fontSize: '1vw'}}>React App</h4>
-              <h6 style={{fontSize: '1vw'}}>Design + Development in progress :P</h6>
-          </div>
-        </div>
+        <h2 style={{color: 'plum'}}>Melanie Wong</h2>
+   
        <div className="intro">
         <div className="about">
           <h4 className="abt-header">About Me</h4>
           <p>Hi there! I'm Melanie, and I'm an undergraduate student studying Computer Science at the University of California in Santa Cruz.</p>
           <Link to="/about">
-            <Button>Learn More</Button>
+            <Button style={{color: 'plum'}}>Learn More</Button>
           </Link>
         </div>
         <div className="about">
           <h4 className="abt-header">About this Site</h4>
-          <p>Welcome to my website! Please feel free to read more <Link to="/about">about</Link> me, or you can check out my <Link to="/resume">resume</Link>, travel <Link to="/Photos">photos</Link>, view site statistics (hopefully soon), or <Link to="/contact">contact</Link> me.</p>
-          <p>Source available on GitHub <a href="https://github.com/mwong775/mwong775.github.io/tree/development" target="_blank">here</a>.</p>
+          <p>Welcome to my website! Please feel free to read more <Link to="/about">about</Link> me, or you can check out my <Link to="/resume">resume</Link>, past <Link to="/projects">projects</Link>, collection of <Link to="/photos">photos</Link>, or <Link to="/contact">contact</Link> me.</p>
+          <p>Source available on GitHub <a href="https://github.com/mwong775/mwong775.github.io/tree/development" target="_blank" rel="noopener noreferrer">here</a>.</p>
         </div>
         </div>
 			</div>
