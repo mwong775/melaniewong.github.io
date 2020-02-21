4672622c9fa35f23add8039e1a18ef4a391add1b
Melanie <melywong@ucsc.edu>
2020-02-21 15:30:42 -0800

    add resume fade in and download button

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,46 +1,23 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import './Resume.css';
+
 
 export class Resume extends React.Component {
 
   render() {
     return (
-      <div>
-      <h2>Resume</h2>
-      <div style={{textAlign: 'center'}}>
-      <div style={{margin: '2%'}}>
-          <iframe src="https://docs.google.com/gview?url=https://github.com/mwong775/mwong775.github.io/raw/development/Resume.pdf&embedded=true" 
-          style={{width:"70%", height:"70vw"}} frameborder="0"></iframe>
-      </div>
-      	</div>
+      <div className="content-wrapper">
+        <h2 className="gradient-font">Resume</h2>
+        <br/>
+        <Button href="https://github.com/mwong775/mwong775.github.io/raw/development/Resume.pdf">Download</Button>
+        <div className="resume-container">
+          <div className="resume-wrapper">
+              <iframe title="Resume" src="https://docs.google.com/gview?url=https://github.com/mwong775/mwong775.github.io/raw/development/Resume.pdf&embedded=true" 
+              frameborder="0"></iframe>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-/*
-    <Tabs 
-          aria-label="simple tabs example"  
-          variant="fullWidth"
-          aria-label="full width tabs example">
-          <Tab label="HTML" />
-          <Tab label="PDF" />
-          <Tab label="Something else" />
-        </Tabs>
-
-    <DatatablePage />
-
-  state = {
-      numPages: null,
-      pageNumber: 1,
-    }
-
-    const { pageNumber, numPages } = this.state;
-    
-        <Document
-          file="Resume.pdf"
-          onLoadSuccess={this.onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <p>Page {pageNumber} of {numPages}</p>
-*/
