dd603484cdf4ab6a5cce847b9ec20f47142bd5ee
Melanie <melywong@ucsc.edu>
2020-02-21 15:34:07 -0800

    update to home intro gradient only

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,23 +7,24 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div className="home">
-        <h2 style={{color: 'plum'}}>Melanie Wong</h2>
-   
-       <div className="intro">
-        <div className="about">
-          <h4 className="abt-header">About Me</h4>
-          <p>Hi there! I'm Melanie, and I'm an undergraduate student studying Computer Science at the University of California in Santa Cruz.</p>
-          <Link to="/about">
-            <Button style={{color: 'plum'}}>Learn More</Button>
-          </Link>
+        <div className="intro">
+          <h2>Melanie Wong</h2>
+        </div>   
+        <div className="about content-wrapper">
+          <div className="about-wrapper">
+            <h4 className="gradient-font">About Me</h4>
+            <p>Hi there! I'm Melanie, and I'm an undergraduate student studying Computer Science at the University of California in Santa Cruz.</p>
+            <Link to="/about">
+              <Button style={{color: 'plum'}}>Learn More</Button>
+            </Link>
+          </div>
+          <div className="about-wrapper">
+            <h4 className="gradient-font">About This Site</h4>
+            <p>Welcome to my website! Please feel free to read more <Link to="/about">about</Link> me, or you can check out my <Link to="/resume">resume</Link>, past <Link to="/projects">projects</Link>, collection of <Link to="/photos">photos</Link>, or <Link to="/contact">contact</Link> me.</p>
+            <p>Source available on GitHub <a href="https://github.com/mwong775/mwong775.github.io/tree/development" target="_blank" rel="noopener noreferrer">here</a>.</p>
+          </div>
         </div>
-        <div className="about">
-          <h4 className="abt-header">About this Site</h4>
-          <p>Welcome to my website! Please feel free to read more <Link to="/about">about</Link> me, or you can check out my <Link to="/resume">resume</Link>, past <Link to="/projects">projects</Link>, collection of <Link to="/photos">photos</Link>, or <Link to="/contact">contact</Link> me.</p>
-          <p>Source available on GitHub <a href="https://github.com/mwong775/mwong775.github.io/tree/development" target="_blank" rel="noopener noreferrer">here</a>.</p>
-        </div>
-        </div>
-			</div>
+      </div>
 			);
 	}
 }
