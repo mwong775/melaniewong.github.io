4118497ea0bfbe198054cfcb34ea4d63c6e41c66
Melanie <melywong@ucsc.edu>
2020-05-23 23:28:01 -0700

    Updates

---
+++ precache-manifest.3dee494cbb264c650ad2e1a0c546eb43.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896c34b7a3ac0a7c68a4f013a4f2766d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a0150f6d7b3360ce332",
+    "url": "/static/css/main.d20e7e4d.chunk.css"
+  },
+  {
+    "revision": "f0c436445d0d192b0703",
+    "url": "/static/js/2.c9a24a74.chunk.js"
+  },
+  {
+    "revision": "6054caf09b601605cdc04f8fcf37fdeb",
+    "url": "/static/js/2.c9a24a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0150f6d7b3360ce332",
+    "url": "/static/js/main.95bd6230.chunk.js"
+  },
+  {
+    "revision": "10e18084de1363b5c050",
+    "url": "/static/js/runtime-main.7cae99bc.js"
+  },
+  {
+    "revision": "9e13cc4e06e57da5cee628e179b4021b",
+    "url": "/static/media/NY_5-15-19_2.9e13cc4e.jpg"
+  },
+  {
+    "revision": "7f31df48fc9be285a9d8bd42fd5f3183",
+    "url": "/static/media/Poster.7f31df48.jpg"
+  },
+  {
+    "revision": "1d0767586b0b8acc2ecdfb270d793383",
+    "url": "/static/media/Tahoe.1d076758.jpg"
+  },
+  {
+    "revision": "8dfb69c70e1a30c841080d1199bcd85b",
+    "url": "/static/media/cinna_stack.8dfb69c7.gif"
+  },
+  {
+    "revision": "66e3768b2dbedf148621672a8c04a3ee",
+    "url": "/static/media/cinna_sup.66e3768b.gif"
+  },
+  {
+    "revision": "c8adcecba492d8afae4c75ee05ca5cf2",
+    "url": "/static/media/giphy.c8adcecb.jpg"
+  },
+  {
+    "revision": "bc5a4e7d6855383c222e06de1f9d970b",
+    "url": "/static/media/old_gold_building.bc5a4e7d.jpg"
+  },
+  {
+    "revision": "727098145b4a88f1f9f55f9c4675184e",
+    "url": "/static/media/pubu.72709814.jpg"
+  },
+  {
+    "revision": "f752cff28f9dad9fbdf03039e05cf068",
+    "url": "/static/media/rocks.f752cff2.jpg"
+  },
+  {
+    "revision": "053ff0d837a0b58b6bc68681bd01484b",
+    "url": "/static/media/sheeep.053ff0d8.jpg"
+  }
+]);
