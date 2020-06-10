a994e425a72b529558231c0322a1ce98805bd918
Melanie <melywong@ucsc.edu>
2020-06-10 13:20:18 -0700

    update jest configs for typescript

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import App from '../App';
 describe('First React component test with Enzyme', () => {
    it('renders without crashing', () => {
       shallow(<App />);
