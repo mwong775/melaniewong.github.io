b6eebaa4f8a8b36fc2225dc4ef18051abb78cfac
Melanie <melywong@ucsc.edu>
2020-06-10 15:00:43 -0700

    git ignore? probably will overwrite on deploy

---
+++ build/precache-manifest.e99e6557d6f75bf22fb95e210f6a5e72.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c18ae93b098e5eb360a4aa9f6f5ecd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c505ed152de75abe909b",
+    "url": "/static/css/main.7535deee.chunk.css"
+  },
+  {
+    "revision": "8e7b4287510e38118e81",
+    "url": "/static/js/2.728e28ae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.728e28ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c505ed152de75abe909b",
+    "url": "/static/js/main.18ddfb42.chunk.js"
+  },
+  {
+    "revision": "10e18084de1363b5c050",
+    "url": "/static/js/runtime-main.7cae99bc.js"
+  },
+  {
+    "revision": "55bf9fdf26057291ca712d9b0efced9d",
+    "url": "/static/media/Melanie_Wong_Resume.55bf9fdf.pdf"
+  },
+  {
+    "revision": "9e13cc4e06e57da5cee628e179b4021b",
+    "url": "/static/media/NY_5-15-19_2.9e13cc4e.jpg"
+  },
+  {
+    "revision": "7f31df48fc9be285a9d8bd42fd5f3183",
+    "url": "/static/media/Poster.7f31df48.jpg"
+  },
+  {
+    "revision": "1d0767586b0b8acc2ecdfb270d793383",
+    "url": "/static/media/Tahoe.1d076758.jpg"
+  },
+  {
+    "revision": "8dfb69c70e1a30c841080d1199bcd85b",
+    "url": "/static/media/cinna_stack.8dfb69c7.gif"
+  },
+  {
+    "revision": "66e3768b2dbedf148621672a8c04a3ee",
+    "url": "/static/media/cinna_sup.66e3768b.gif"
+  },
+  {
+    "revision": "c8adcecba492d8afae4c75ee05ca5cf2",
+    "url": "/static/media/giphy.c8adcecb.jpg"
+  },
+  {
+    "revision": "bc5a4e7d6855383c222e06de1f9d970b",
+    "url": "/static/media/old_gold_building.bc5a4e7d.jpg"
+  },
+  {
+    "revision": "727098145b4a88f1f9f55f9c4675184e",
+    "url": "/static/media/pubu.72709814.jpg"
+  },
+  {
+    "revision": "f752cff28f9dad9fbdf03039e05cf068",
+    "url": "/static/media/rocks.f752cff2.jpg"
+  },
+  {
+    "revision": "053ff0d837a0b58b6bc68681bd01484b",
+    "url": "/static/media/sheeep.053ff0d8.jpg"
+  }
+]);
